00c4da289c265b017ba311230d779e803cb019c9
Nikhil Dixit <nvdixit@ncsu.edu>
2021-05-26 16:29:51 -0500

    Added MongoDB to project

--- Tracksaction/src/main/java/nvdixit/Tracksaction/main/Main.java
+++ Tracksaction/src/main/java/nvdixit/Tracksaction/main/Main.java
@@ -2,10 +2,20 @@ package nvdixit.Tracksaction.main;
 
 import nvdixit.Tracksaction.Manager.CreditCardManager;
 
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ConnectionString;
+import com.mongodb.ServerAddress;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoClientSettings;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		MongoClient mongoClient = MongoClients.create();
+		 
 		CreditCardManager manager = new CreditCardManager();
 		
 	}
