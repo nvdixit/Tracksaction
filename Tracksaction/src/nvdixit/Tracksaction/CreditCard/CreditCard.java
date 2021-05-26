899bee813e559235c97e06052da5228908c83767
Nikhil Dixit <nvdixit@ncsu.edu>
2021-05-26 12:17:25 -0500

    Implemented CreditCardManager class

--- Tracksaction/src/nvdixit/Tracksaction/CreditCard/CreditCard.java
+++ Tracksaction/src/nvdixit/Tracksaction/CreditCard/CreditCard.java
@@ -67,27 +67,35 @@ public class CreditCard {
 		
 		return null;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((transactions == null) ? 0 : transactions.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CreditCard other = (CreditCard) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (transactions == null) {
+			if (other.transactions != null)
+				return false;
+		} else if (!transactions.equals(other.transactions))
+			return false;
+		return true;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ Tracksaction/src/nvdixit/Tracksaction/Manager/CreditCardManager.java
@@ -0,0 +1,80 @@
+package nvdixit.Tracksaction.Manager;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import nvdixit.Tracksaction.CreditCard.CreditCard;
+import nvdixit.Tracksaction.Transaction.Transaction;
+
+/**
+ * Manages all CreditCards
+ * @author nikhildixit
+ *
+ */
+public class CreditCardManager {
+
+	/** All credit cards the user has */
+	private ArrayList<CreditCard> creditCards;
+	
+	/**
+	 * Constructs a new CreditCardManager
+	 */
+	public CreditCardManager() {
+		creditCards = new ArrayList<CreditCard>();
+	}
+	
+	/**
+	 * Adds a CreditCard to the manager
+	 * @param card the card to add
+	 */
+	public void addCreditCard(CreditCard card) {
+		creditCards.add(card);
+	}
+	
+	/**
+	 * Removes a CreditCard from the Manager
+	 * @param card the card to remove
+	 * @return the removed card
+	 */
+	public CreditCard removeCreditCard(CreditCard card) {
+		Iterator<CreditCard> it = creditCards.iterator();
+		
+		while(it.hasNext()) {
+			CreditCard c = it.next();
+			
+			if(card.equals(c)) {
+				return c;
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Adds a Transaction to the given CreditCard
+	 * @param card the card to add to 
+	 * @param transaction the transaction to add
+	 */
+	public void addTransactionToCard(CreditCard card, Transaction transaction) {
+		card.addTransaction(transaction);
+	}
+	
+	/**
+	 * Removes a Transaction from the given CC
+	 * @param card the CC to remove from
+	 * @param transaction the transaction to remove
+	 */
+	public Transaction removeTransactionFromCard(CreditCard card, Transaction transaction) {
+		Iterator<CreditCard> it = creditCards.iterator();
+		
+		while(it.hasNext()) {
+			CreditCard c = it.next();
+			
+			if(card.equals(c)) {
+				return c.removeTransaction(transaction);
+			}
+		}
+		
+		return null;
+	}
+}
