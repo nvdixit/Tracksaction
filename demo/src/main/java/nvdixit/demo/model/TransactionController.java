3f18ec0b4a946fc6e5d86faed1b119a69ba5a930
Nikhil Dixit <nvdixit@ncsu.edu>
2021-06-12 11:58:48 -0500

    Transaction endpoint established

--- demo/src/main/java/nvdixit/demo/model/TransactionController.java
+++ demo/src/main/java/nvdixit/demo/model/TransactionController.java
@@ -6,7 +6,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController	
@@ -24,5 +27,10 @@ public class TransactionController {
 	@GetMapping
 	public List<Transaction> getTransactions() {
 		return transactionService.getTransactions();
+	}
+	
+	@PostMapping(path = "{transactionName}/transaction/upload")
+	public void uploadTransaction(@RequestParam("name") String name) {
+		transactionService.uploadTransaction(name, 0.0);
 	}
 }
--- demo/src/main/java/nvdixit/demo/model/TransactionService.java
+++ demo/src/main/java/nvdixit/demo/model/TransactionService.java
@@ -5,17 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import nvdixit.demo.filestore.FileStore;
+
 @Service
 public class TransactionService {
 	
 	private final TransactionDataAccessService transactionDataAccessService;
+	private final FileStore fileStore;
 	
 	@Autowired
-	public TransactionService(TransactionDataAccessService transactionDataAccessService) {
+	public TransactionService(TransactionDataAccessService transactionDataAccessService, FileStore fileStore) {
 		this.transactionDataAccessService = transactionDataAccessService;
+		this.fileStore = fileStore;
 	}
 	
 	List<Transaction> getTransactions() {
 		return this.transactionDataAccessService.getTransactions();
+	}
+
+	public void uploadTransaction(String name, double amount) {
+		System.out.println("Name: " + name);
 	}
 }
